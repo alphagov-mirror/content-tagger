46f5d0fa2dbd4b838c92c0118e4e273f7ae4ef5b
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-10-17 11:29:07 +0100

    Allow taxon payload to handle non-'en' locales
    In preparation to allow the Brexit taxon to be published with a 'cy' locale, we
    need to ensure that the taxon payload that is built to send to the Publishing
    API can handle non-'en' locales, as currently 'en' is the hardcoded locale
    value. 'en' is provided as a default locale as all other taxons only need to be
    published in that locale currently, however allowing an optional locale to be
    provided will enable taxons to be published with a different locale if
    necessary in future.

--- app/services/taxonomy/build_taxon_payload.rb
+++ app/services/taxonomy/build_taxon_payload.rb
@@ -4,13 +4,14 @@ module Taxonomy
       @taxon = taxon
     end
 
-    def self.call(taxon:)
-      new(taxon).build
+    def self.call(taxon:, locale: "en")
+      new(taxon).build(locale)
     end
 
-    def build
+    def build(locale)
+      path = locale == "en" ? base_path : base_path + ".#{locale}"
       {
-        base_path: base_path,
+        base_path: path,
         document_type: "taxon",
         schema_name: "taxon",
         title: title,
@@ -18,14 +19,14 @@ module Taxonomy
         publishing_app: "content-tagger",
         rendering_app: "collections",
         public_updated_at: Time.now.iso8601,
-        locale: "en",
+        locale: locale,
         details: {
           internal_name: internal_name,
           notes_for_editors: notes_for_editors,
           visible_to_departmental_editors: visible_to_departmental_editors,
         },
         routes: [
-          { path: base_path, type: "exact" },
+          { path: path, type: "exact" },
         ],
         update_type: "major",
         phase: phase,
